21e2cf651a5cea0abe64a0eb780190d7cbc09d22
amaiable <atmai2002@gmail.com>
2025-08-11 12:21:53 +1000

    feat: added password validation for both registration and password change

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
+
+
+def is_valid_password(password: str) -> str:
+    if len(password) < 6:
+        raise ValueError("Password must be at least 6 characters long")
+    return password
 
 
 class UserCreateReq(BaseModel):
     email: EmailStr
     password: str
+
+    @field_validator("password")
+    def validate_password(cls, password: str) -> str:
+        return is_valid_password(password)
 
 
 class UserHashed(BaseModel):
@@ -20,6 +30,11 @@ class UserModel(BaseModel):
 class UserRes(BaseModel):
     email: EmailStr
 
+
 class ChangePasswordReq(BaseModel):
     old_password: str
     new_password: str
+
+    @field_validator("new_password")
+    def validate_new_password(cls, new_password: str) -> str:
+        return is_valid_password(new_password)
