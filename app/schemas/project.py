fba653881b129409b05016c81672e8de9ca48ddd
amaiable <atmai2002@gmail.com>
2025-08-19 15:10:04 +1000

    feat: added schema for endpoint responses

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 from typing import List
 
+from app.schemas.todo import Todo
+
 
 class TodoStatus(BaseModel):
     id: str
@@ -22,9 +24,17 @@ class GetProjectRequest(BaseModel):
     pass
 
 
+class GetProjectResponse(BaseModel):
+    project: Project
+
+
 # Will pass project_id through path
 class GetTodoItemsForProjectRequest(BaseModel):
     pass
+
+
+class GetTodoItemsForProjectResponse(BaseModel):
+    todos: List[Todo]
 
 
 # Will pass project_id through path
@@ -33,15 +43,27 @@ class AddTodoStatusRequest(BaseModel):
     name: str
 
 
+class AddTodoStatusResponse(BaseModel):
+    pass
+
+
 # Will pass project_id through path
 # May be able to pass status id through path as well
 class DeleteTodoStatusRequest(BaseModel):
     todo_status_id: str
 
 
+class DeleteTodoStatusResponse(BaseModel):
+    pass
+
+
 # Will pass project_id through path
 class ReorderTodoStatusRequest(BaseModel):
     new_todo_statuses: List[str]
+
+
+class ReorderTodoStatusResponse(BaseModel):
+    pass
 
 
 # Will pass project_id through path
@@ -51,7 +73,15 @@ class AddTodoRequest(BaseModel):
     todo_status_id: str
 
 
+class AddTodoResponse(BaseModel):
+    pass
+
+
 # Will pass project_id through path
 # May be worth considering just using the path for the todo_id as well
 class DeleteTodoRequest(BaseModel):
     todo_id: str
+
+
+class DeleteTodoResponse(BaseModel):
+    pass
