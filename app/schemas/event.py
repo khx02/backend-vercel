7a4371c006d4d66b3df5847b907f0ed580791297
amaiable <atmai2002@gmail.com>
2025-08-29 09:17:40 +1000

    feat: added endpoint schemas for event rsvp system

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,10 +1,24 @@
-from pydantic import BaseModel
+from enum import Enum
+from typing import List
+from pydantic import BaseModel, EmailStr
+
+
+class RSVPStatus(Enum):
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    DECLINED = "declined"
+
+
+class RSVP(BaseModel):
+    id: str
+    rsvp_status: RSVPStatus
 
 
 class Event(BaseModel):
     id: str
     name: str
     description: str
+    rsvp_ids: List[str]
 
 
 # Pass event_id through path
@@ -14,3 +28,32 @@ class GetEventRequest(BaseModel):
 
 class GetEventResponse(BaseModel):
     event: Event
+
+
+# For clubs to send email to event attendees
+# Pass event_id through path
+class SendRSVPEmailRequest(BaseModel):
+    email: EmailStr
+
+
+class SendRSVPEmailResponse(BaseModel):
+    pass
+
+
+# For event attendees to confirm or deny event attendance
+# Pass rsvp_id through path
+class ReplyRSVPRequest(BaseModel):
+    rsvp_status: RSVPStatus
+
+
+class ReplyRSVPResponse(BaseModel):
+    pass
+
+
+# Pass event_id through path
+class GetEventRSVPsRequest(BaseModel):
+    pass
+
+
+class GetEventRSVPsResponse(BaseModel):
+    rsvps: List[RSVP]
