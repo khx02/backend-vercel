53ac3f2015fce799f00d1ebfa5182f2e94597ba7
amaiable <atmai2002@gmail.com>
2025-08-29 08:51:04 +1000

    fix: small change to use is None vs checking for falsy

--- app/service/event.py
+++ app/service/event.py
@@ -8,7 +8,7 @@ from app.schemas.event import Event
 async def get_event_service(event_id: str, db: AsyncDatabase) -> Event:
 
     event_in_db_dict = await db_get_event_or_none(event_id, db)
-    if not event_in_db_dict:
+    if event_in_db_dict is None:
         raise HTTPException(
             status_code=404,
             detail=f"Could not find an event for this id: id={event_id}",
